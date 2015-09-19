73b8b87edfe28d74a52a73c3fac9ce93cb09b4b5
Nathaniel Grudzinski <n.grudzinski@siu.edu>
2015-09-19 03:22:26 -0500

    finished_file_single_byte_XOR.py

--- single_byte_XOR.py
+++ single_byte_XOR.py
@@ -1,25 +1,25 @@
-encoded_hex = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
-
-scale = 16
-length = len(encoded_hex)
-num_of_bits = length * 4
-
-binary_string = bin(int(encoded_hex, scale))[2:].zfill(num_of_bits)
-binary_arr = list(binary_string)
+FIXED_HEX = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
 
 
 def detect_words(bin_string):
     cnt = 0
     for i in range(0, len(bin_string), 8):
         dec = int(bin_string[i:i+8], 2)
-        if dec in range(32, 123):
+        if dec in range(32, 127):
             cnt += 1
     if cnt == len(bin_string)/8:
         return 1
     return 0
 
 
-def xor_2_strings(binary_array):
+def xor_2_strings(encoded_hex):
+    scale = 16
+    length = len(encoded_hex)
+    num_of_bits = length * 4
+
+    binary_string = bin(int(encoded_hex, scale))[2:].zfill(num_of_bits)
+    binary_array = list(binary_string)
+
     for i in range(0, 256):
         # this next line creates byte sized strings of 0 to 256 in binary
         bin_i8 = bin(i)[2:].zfill(8)
@@ -33,4 +33,5 @@ def xor_2_strings(binary_array):
         if detect_words(xor_string) == 1:
             print(chr(i) + "    " + ''.join(chr(int(xor_string[i:i+8], 2)) for i in range(0, len(xor_string), 8)))
 
-xor_2_strings(binary_arr)
+if __name__ == "__main__":
+    xor_2_strings(FIXED_HEX)
